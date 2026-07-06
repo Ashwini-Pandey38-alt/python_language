2c94acbfce03ea96fd1fe3a47173103dfeca65e4
ashwini pandey <ashwinipandit38@gmail.com>
2026-07-06 07:22:56 +0530

    updateing with new code

--- basic.py
+++ basic.py
@@ -2,3 +2,18 @@ a=10
 b=20
 sum=a+b
 print(sum)
+# WAP that asks the user for their name and age   
+name=input('enter your name plz')
+age=int(input ('enter your age plz'))
+print('so ur age is',name,'and age is',age)
+
+# Take two numbers as input from the user and print 
+#thier sum,difference,product, and quotient
+
+a=int(input("enter the value of a="))
+b=int(input("enter the value of b="))
+s=a+b
+d=a-b 
+p=a*b
+q=a/b
+print("sum,difference,product, and quotient","====",s,d,p,q)
