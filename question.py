f8e8f47f754d32dc1094e8e7986f4d84cb9f9ddb
ashwini pandey <ashwinipandit38@gmail.com>
2026-07-06 07:40:06 +0530

    conversion question

--- question.py
+++ question.py
@@ -1,1 +1,12 @@
 print("hello world")
+
+#. Ask the user to enter two integers and one float. Convert them all to floats 
+#and print their average.
+n1=int(input("enter the value of the number n1==="))
+n2=float(input("enter the value of the number n2==="))
+n3=int(input("enter the value of the number n3==="))
+a=float(n1)
+b=float(n2)
+c=float(n3)
+avg=(a+b+c)/3
+print(avg)
